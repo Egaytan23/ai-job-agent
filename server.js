7fef7e7c5c1ace5277eb188d038269d1ebc25acf
Egaytan23 <elvisgaitan23@gmail.com>
2026-06-26 20:32:21 -0400

    Update server.js
    debug environment var

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const cors = require("cors");
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY //Go grab the API key from my environment variables NOT from the code
 });
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
 //creates a postgreSQL connection pool ie "my backend's connection to the database"
 const pool = new Pool({
     user: process.env.DB_USER,
